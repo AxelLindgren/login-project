fa9c313a5b585e7dc93c1ff58422c896f782a448
AxelLindgren <axelwlindgren@gmail.com>
2025-01-09 13:22:20 +0100

    session

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
-      httpOnly: true,
+      httpOnly: false,
       secure: true,
       sameSite: "none",
     },
